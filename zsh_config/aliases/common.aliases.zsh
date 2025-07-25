e11425aedbf4e8313802f5f32479baa928aac23f
lipeng.han <hlp593628242@gmail.com>
2025-07-25 16:47:43 +0800

    feat: custom config for me

--- zsh_config/aliases/common.aliases.zsh
+++ zsh_config/aliases/common.aliases.zsh
@@ -1,5 +1,10 @@
 # ~/.config/zsh/aliases.sh
 
+alias proxy-sw="source ~/.bin/script/proxy-switch"
+alias gemini-cli="npx @google/gemini-cli"
+alias rlzsh="source ~/.zshrc"
+alias tfp="cd ~/IdeaProjects/"
+
 # Add your custom aliases here.
 #
 # Aliases allow you to create shortcuts for longer commands.
---
+++ zsh_config/plugins/my_plugins.zsh
@@ -0,0 +1,3 @@
+zinit light zsh-users/zsh-syntax-highlighting
+zinit light zsh-users/zsh-autosuggestions
+zinit light zsh-users/zsh-completions
